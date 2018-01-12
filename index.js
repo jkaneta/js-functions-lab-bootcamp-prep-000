34a09ef80093dc03aacc317b1ef225016295cf7e
J <flatironschool@2joyce.com>
2018-01-12 03:36:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(name, number) {
-  return `It\'s ${days} days until ${holiday}!`;
+  return `It\'s ${number} days until ${holiday}!`;
 }
